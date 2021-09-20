b9c1fe67f58aa1753bdacfba5f4341e16ca4d76c
hyewoncc <hyewoncc@gmail.com>
2021-09-20 23:42:49 +0900

    Fix delete image index array delete string array to manipulate instances with different image numbers

--- src/components/common/BeerCard/BeerCard.js
+++ src/components/common/BeerCard/BeerCard.js
@@ -5,9 +5,7 @@ function BeerCard() {
 
     const [MouseX, setMouseX] = useState(0);
     const [MouseY, setMouseY] = useState(0);
-    const [ImageArr, setImageArr] = useState(['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', 
-                                            '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25']);
-    const [ImageIndex, setImageIndex] = useState(0);
+    const [ImageIndex, setImageIndex] = useState(1);
 
     const initMousePosition = (e) => {
         setMouseX(e.screenX);
@@ -15,17 +13,17 @@ function BeerCard() {
 
     const detectMouseMove = (e) => {
         if((MouseX + 10) < e.screenX) {
-            if(ImageIndex < 24) {
+            if(ImageIndex < 25) {
                 setImageIndex(ImageIndex + 1);
-            }else if(ImageIndex === 24) {
+            }else if(ImageIndex === 25) {
                 setImageIndex(0);
             }
             setMouseX(e.screenX);
         }
         if((MouseX - 10) > e.screenX) {
-            if(ImageIndex > 0) {
+            if(ImageIndex > 1) {
                 setImageIndex(ImageIndex - 1);
-            }else if(ImageIndex === 0){
+            }else if(ImageIndex === 1){
                 setImageIndex(24);
             }
             setMouseX(e.screenX);
@@ -37,7 +35,7 @@ function BeerCard() {
     return (
         <div className="beercard-wrap">
             <div className="beercard" onMouseEnter={initMousePosition} onMouseMove={detectMouseMove}>
-                <img src={img_url + ImageArr[ImageIndex] + '.png'}/>
+                <img src={img_url + ImageIndex + '.png'}/>
             </div>
         </div>
     )
