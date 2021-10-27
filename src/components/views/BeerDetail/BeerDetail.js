f20a58531829c427fd180e1a4aff4241fa86d04c
hyewoncc <hyewoncc@gmail.com>
2021-10-27 20:52:48 +0900

    Add beer detail page layout and contents

--- src/components/views/BeerDetail/BeerDetail.js
+++ src/components/views/BeerDetail/BeerDetail.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { API_URL } from '../../Config';
+import { BEERCARD_IMG_URL } from '../../Config';
 import './BeerDetail.css';
 
 function BeerDetail(props) {
@@ -13,20 +14,36 @@ function BeerDetail(props) {
             .then(response => response.json())
             .then(response => {
                 setBeer(response);
+                console.log(response);
             })
     }, [])
 
     return (
         <div className="info-wrap">
             <div className="info-container">
-                <div className="beer-image">
-
+                <div className="low">
+                    <div className="beer-image">
+                        <img src={'../' + BEERCARD_IMG_URL + Beer.name + '/' + Beer.name + '1.png'}/>
+                    </div>
+                    <div className="beer-info">
+                        <div className="low-text beer-name">
+                            {Beer.name}
+                        </div>
+                        <div className="low-text beer-type">
+                            {Beer.beerType}
+                        </div>
+                        <div className="low-text beer-country">
+                            {Beer.country}
+                        </div>
+                        <div className="low-text beer-description">
+                            {Beer.description}
+                        </div>
+                    </div>
                 </div>
-                <div className="beer-info">
+                <div className="low">
+                    <div className="comment-container">
 
-                </div>
-                <div className="comment-container">
-
+                    </div>
                 </div>
             </div>
         </div>
