f8fdc398c911fe4e31f88ec6a7f4a30a7350dcea
hyewoncc <hyewoncc@gmail.com>
2021-10-29 23:56:09 +0900

    Add new comment

--- src/components/common/Comments/CommentsList.js
+++ src/components/common/Comments/CommentsList.js
@@ -1,24 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import { API_URL } from '../../Config';
+import './CommentsList.css';
 
 function CommentsList(props) {
     const [Comments, setComments] = useState([]);
 
     useEffect(() => {
         const endpoint = `${API_URL}comments/?beerId=${props.beerId}`;
-        
+        fetchComments(endpoint);
+    }, [])
+
+    const fetchComments = (endpoint) => {
         fetch(endpoint)
             .then(response => response.json())
             .then(response => {
                 setComments(response._embedded.commentDtoes);
-            })
-    }, [])
+        })
+    }
 
     return (
-        <div>
-            {Comments && Comments.map((comment, index) => (
-                <p>{comment.name} : {comment.content}</p>
-            ))}
+        <div className="comment-wrap low wrap">
+            <div className="comments low wrap">
+                {Comments && Comments.map((comment, index) => (
+                    <div className="comment low">
+                        <p>{comment.name} : {comment.content}</p>
+                    </div>
+                ))}
+            </div>
+            <div className="new-comment low wrap">
+                <div>
+
+                </div>
+                <input type="text" className="nickname-input"></input>
+                <div>
+                    
+                </div>
+                <input type="text" className="comment-input"></input>
+                <button>등록</button>
+            </div>
         </div>
     )
 }
