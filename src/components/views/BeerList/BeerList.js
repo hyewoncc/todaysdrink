358ba74a228666f456dd818c01a813ec92df7194
hyewoncc <hyewoncc@gmail.com>
2021-12-09 12:37:31 +0900

    Add beer search option boxes

--- src/components/views/BeerList/BeerList.js
+++ src/components/views/BeerList/BeerList.js
@@ -8,9 +8,15 @@ function BeerList() {
 
     const [Beers, setBeers] = useState([]);
     const [Page, setPage] = useState(0);
+    const [Option, setOption] = useState([[]]);
+    const likeOption = [["많은 순으로", "desc"], ["적은 순으로", "asc"]];
+    const alcoholOption = [["높은 순으로", "desc"], ["낮은 순으로", "asc"]];
+    const typeOption = [["라거", "lager"], ["스타우트", "stout"], ["IPA", "IPA"]];
+    const countryOption = [["한국", "KOR"], ["일본", "JPN"], ["독일", "DEU"]];
 
     useEffect(() => {
         const endpoint = `${API_URL}beers`;
+        setOption(likeOption);
         fetchBeers(endpoint);
     }, [])
 
@@ -24,8 +30,43 @@ function BeerList() {
             })
     }
 
+    const onOptionSelectHandler = (e) => {
+        let option = e.target.value;
+
+        switch(option) {
+            case "like":
+                setOption(likeOption);
+                break;
+            case "alcohol":
+                setOption(alcoholOption);
+                break;
+            case "type":
+                setOption(typeOption);
+                break;
+            case "country":
+                setOption(countryOption);
+                break;
+            default:
+                break;
+        }
+    }
+
     return (
         <div className="beerlist-wrap">
+            <div className="beerlist-option">
+                <span>맥주 진열은</span>
+                <select onChange={onOptionSelectHandler}>
+                    <option key="like" value="like">좋아요</option>
+                    <option key="alcohol" value="alcohol">도수</option>
+                    <option key="type" value="type">종류</option>
+                    <option key="country" value="country">국가</option>
+                </select>
+                <select>
+                    {Option.map((op, index) => (
+                        <option value={op[1]}>{op[0]}</option>
+                    ))}
+                </select>
+            </div>
             <div className="beerlist-container">
                 {Beers && Beers.map((beer, index) => (
                     <React.Fragment key = {index}>
