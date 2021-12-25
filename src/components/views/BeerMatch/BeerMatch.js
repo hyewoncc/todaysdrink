d331fa5351b0d785bfbcea70c6c5960f81683676
hyewoncc <hyewoncc@gmail.com>
2021-12-25 23:55:49 +0900

    Add match page and data

--- src/components/views/BeerMatch/BeerMatch.js
+++ src/components/views/BeerMatch/BeerMatch.js
@@ -1,19 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import './BeerMatch.css';
+import MatchRawData from "../../MatchData.json";
 
 function BeerMatch() {
+    
+    const [MatchData, setMatchData] = useState(MatchRawData);
+
     return (
         <div className='beermatch-wrap'>
             <div className='beerimage-wrap'>
 
             </div>
             <div className='match-sheet'>
-                <div className='question'>
-
+                <div className='match-container'>
+                    {MatchData && MatchData.map((data, index) => (
+                        <div className='question'>
+                            <div className='question-title'>
+                                <p>{data.title}</p>
+                            </div>
+                            <div className='question-answers'>
+                                {data.answers && data.answers.map((answer, index) => (
+                                    <div className='answer'>
+                                        <p>{answer.explain}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
                 </div>
-                <div className='items'>
 
-                </div>
             </div>
         </div>
     )
