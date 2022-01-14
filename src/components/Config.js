935aab0ff7777a7b0b01d88373a1244668e75ff5
hyewoncc <hyewoncc@gmail.com>
2022-01-14 21:38:42 +0900

    Fix file path

--- src/components/Config.js
+++ src/components/Config.js
@@ -2,4 +2,4 @@ export const API_URL = '/api/';
 
 export const BEERCARD_IMG_URL = `${process.env.PUBLIC_URL}/images/beer_small/`;
 export const ICON_IMG_URL = `${process.env.PUBLIC_URL}/images/icon/`;
-
+export const IMG_URL = `${process.env.PUBLIC_URL}/images/`;
