6a49ff6a87487fffe3428a303cb7f861cbae9525
hyewoncc <hyewoncc@gmail.com>
2022-01-25 14:52:37 +0900

    Add change description when type clicked

--- src/components/views/BeerTypes/BeerTypes.js
+++ src/components/views/BeerTypes/BeerTypes.js
@@ -5,20 +5,27 @@ import './BeerTypes.css';
 function BeerTypes() {
 
     const [TypesData, setTypesData] = useState([]);
+    const [TypeData, setTypeData] = useState();
 
     useEffect(() => {
         setTypesData(TypesRawData);
     }, [])
-
 
     return (
         <div className='types-container'>
             <div className='types-list-wrap'>
                 <ul>
                     {TypesData.map((data, i) => (
-                        <li>{data.type}</li>
+                        <li onClick={() => setTypeData(data.explain)}>{data.type}</li>
                     ))}
                 </ul>
+            </div>    
+            <div className='types-explain-wrap'>
+                <p>
+                    {TypeData && TypeData.split('\\n').map(line => (
+                        <span>{line}<br/></span>
+                    ))}
+                </p>
             </div>
         </div>
     )
